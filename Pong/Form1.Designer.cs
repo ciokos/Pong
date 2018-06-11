2c7b44b1ceff1f2bcb93110734c6c43af3758b8a
ciokos <Ciokos23>
2018-06-11 22:34:28 +0200

    initial post

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ball = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.leftpadle = new System.Windows.Forms.Panel();
+            this.rightpadle = new System.Windows.Forms.Panel();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.leftscore = new System.Windows.Forms.Label();
+            this.rightscore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ball.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ball.Location = new System.Drawing.Point(380, 193);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // leftpadle
+            // 
+            this.leftpadle.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.leftpadle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.leftpadle.Location = new System.Drawing.Point(30, 164);
+            this.leftpadle.Name = "leftpadle";
+            this.leftpadle.Size = new System.Drawing.Size(30, 100);
+            this.leftpadle.TabIndex = 2;
+            // 
+            // rightpadle
+            // 
+            this.rightpadle.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.rightpadle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rightpadle.Location = new System.Drawing.Point(740, 164);
+            this.rightpadle.Name = "rightpadle";
+            this.rightpadle.Size = new System.Drawing.Size(30, 100);
+            this.rightpadle.TabIndex = 3;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(307, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // leftscore
+            // 
+            this.leftscore.AutoSize = true;
+            this.leftscore.Cursor = System.Windows.Forms.Cursors.Default;
+            this.leftscore.Font = new System.Drawing.Font("Century Gothic", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftscore.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.leftscore.Location = new System.Drawing.Point(109, 57);
+            this.leftscore.Name = "leftscore";
+            this.leftscore.Size = new System.Drawing.Size(281, 318);
+            this.leftscore.TabIndex = 7;
+            this.leftscore.Text = "0";
+            this.leftscore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rightscore
+            // 
+            this.rightscore.AutoSize = true;
+            this.rightscore.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rightscore.Font = new System.Drawing.Font("Century Gothic", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightscore.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.rightscore.Location = new System.Drawing.Point(434, 57);
+            this.rightscore.Name = "rightscore";
+            this.rightscore.Size = new System.Drawing.Size(281, 318);
+            this.rightscore.TabIndex = 8;
+            this.rightscore.Text = "0";
+            this.rightscore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rightscore);
+            this.Controls.Add(this.leftscore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rightpadle);
+            this.Controls.Add(this.leftpadle);
+            this.Controls.Add(this.ball);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(808, 477);
+            this.MinimumSize = new System.Drawing.Size(808, 477);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Panel leftpadle;
+        private System.Windows.Forms.Panel rightpadle;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label leftscore;
+        private System.Windows.Forms.Label rightscore;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        private int leftpadlemove = 0;
+        private int rightpadlemove = 0;
+        private int padlespeed = 5;
+        private int ballxspeed = 3;
+        private int ballyspeed = 1;
+        private int ballydir = 1;
+        private int ballxdir = 1;
+        private bool delay = true;
+        private int score1 = 0;
+        private int score2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            setup_score();
+            reset_ball();
+        }
+
+        private void setup_score()
+        {
+            leftscore.Location = new Point((this.Width - leftscore.Width - rightscore.Width) / 3 + 20, (this.Height - leftscore.Height) / 2 - 23);
+            rightscore.Location = new Point(leftscore.Location.X*2 + leftscore.Width - 50, leftscore.Location.Y);
+            ball.BringToFront();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (leftpadle.Top <= 0 && leftpadlemove == -1)
+                return;
+            if (leftpadle.Bottom >= this.Height - 27 && leftpadlemove == 1)
+                return;
+            move_padle(leftpadle, leftpadlemove);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (rightpadle.Top <= 0 && rightpadlemove == -1)
+                return;
+            if (rightpadle.Bottom >= this.Height - 27 && rightpadlemove == 1)
+                return;
+            move_padle(rightpadle, rightpadlemove);
+        }
+
+        private void move_padle(Panel p, int arg)
+        {
+            p.Location = new Point(p.Location.X, p.Location.Y + padlespeed * arg);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                rightpadlemove = 1;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                rightpadlemove = -1;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                leftpadlemove = 1;
+            }
+            else if (e.KeyCode == Keys.W)
+            {
+                leftpadlemove = -1;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down && rightpadlemove != -1)
+            {
+                rightpadlemove = 0;
+            }
+            else if (e.KeyCode == Keys.Up && rightpadlemove != 1)
+            {
+                rightpadlemove = 0;
+            }
+            else if (e.KeyCode == Keys.S && leftpadlemove != -1)
+            {
+                leftpadlemove = 0;
+            }
+            else if (e.KeyCode == Keys.W && leftpadlemove != 1)
+            {
+                leftpadlemove = 0;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            bounce();
+            move_ball();
+        }
+
+        private void move_ball()
+        {
+            ball.Location = new Point(ball.Location.X + ballxspeed*ballxdir, ball.Location.Y + ballyspeed*ballydir);
+        }
+
+        private void reset_ball()
+        {
+            Random rnd = new Random();
+            ballyspeed = rnd.Next(0, 5);
+            ballxdir = rnd.Next(0, 2);
+            if (ballxdir == 0)
+                ballxdir = -1;
+            ballydir = rnd.Next(0, 2);
+            if (ballydir == 0)
+                ballydir = -1;
+
+
+            ball.Location = new Point(this.Width / 2 - ball.Width/2, this.Height / 2 - ball.Height/2);
+            ballxspeed = 3;
+
+            leftscore.Text = score1.ToString();
+            rightscore.Text = score2.ToString();
+        }
+
+        private void bounce()
+        {
+            if (ball.Left <= leftpadle.Right && ball.Left >= leftpadle.Right - ballxspeed && ball.Bottom > leftpadle.Top && ball.Top < leftpadle.Bottom)
+            {
+                ballxdir = ballxdir * -1;
+                ballyspeed = ballyspeed + leftpadlemove * ballydir;
+                if (delay)
+                {
+                    ballxspeed += 1;
+                    delay = false;
+                }
+                else
+                    delay = true;
+            }
+                
+
+            else if (ball.Right >= rightpadle.Left && ball.Right <= rightpadle.Left + ballxspeed && ball.Bottom > rightpadle.Top && ball.Top < rightpadle.Bottom)
+            {
+                ballxdir = ballxdir * -1;
+                ballyspeed = ballyspeed + rightpadlemove * ballydir;
+                if (delay)
+                {
+                    ballxspeed += 1;
+                    delay = false;
+                }
+                else
+                    delay = true;
+            }
+                
+
+            if (ball.Bottom >= this.Height-27)
+                ballydir = ballydir * -1;
+
+            else if (ball.Top <= 0)
+                ballydir = ballydir * -1;
+
+            if(ball.Right > this.Width)
+            {
+                score1 += 1;
+                reset_ball();
+            }
+            else if (ball.Left < 0)
+            {
+                score2 += 1;
+                reset_ball();
+            }
+        }
+    }
+}
